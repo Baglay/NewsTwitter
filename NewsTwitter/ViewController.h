490709b2309152ea126b95e188724061edd9523f
Student <student@macbook6zh3qd.local>
2017-03-11 10:09:13 +0700

    Initial Commit

---
+++ NewsTwitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsTwitter
+//
+//  Created by Student on 11.03.17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewsTwitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewsTwitter
+//
+//  Created by Student on 11.03.17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
